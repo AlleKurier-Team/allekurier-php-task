6eed65e09f534af1d5c7a0638a6ef246112e386e
Kasper <42995213+tosterakakwapisz@users.noreply.github.com>
2024-11-19 14:33:49 +0100

    Add saving user to db, also update interface and repository with new methods for easier development

--- src/Core/User/Infrastructure/Persistance/DoctrineUserRepository.php
+++ src/Core/User/Infrastructure/Persistance/DoctrineUserRepository.php
@@ -7,11 +7,14 @@ use App\Core\User\Domain\Repository\UserRepositoryInterface;
 use App\Core\User\Domain\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\NonUniqueResultException;
+use Psr\EventDispatcher\EventDispatcherInterface;
 
 class DoctrineUserRepository implements UserRepositoryInterface
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly EventDispatcherInterface $eventDispatcher,
+    ) {
     }
 
     /**
@@ -26,12 +29,28 @@ class DoctrineUserRepository implements UserRepositoryInterface
             ->setParameter(':user_email', $email)
             ->setMaxResults(1)
             ->getQuery()
-            ->getOneOrNullResult();
+            ->getOneOrNullResult()
+        ;
 
         if (null === $user) {
             throw new UserNotFoundException('Użytkownik nie istnieje');
         }
 
         return $user;
+    }
+
+    public function save(User $user): void
+    {
+        $this->entityManager->persist($user);
+
+        $events = $user->pullEvents();
+        foreach ($events as $event) {
+            $this->eventDispatcher->dispatch($event);
+        }
+    }
+
+    public function flush(): void
+    {
+        $this->entityManager->flush();
     }
 }
